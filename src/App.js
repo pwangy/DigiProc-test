ae634d2ac8b44f728a9ef6cc20f18a2992644bed
Peggy <64912576+pwangy@users.noreply.github.com>
2022-05-20 23:40:38 +0200

    mapping data from json works

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
 
+import posts from './data/comments.json'
+
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <article>
+        <h1>Changelog</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Marius vitae sapien vitae odio viverra placerat. Suspendisse velit leo, placer at ac porttitor et, elementum in enim.</p>
+      </article>
+      <article>
+        {posts.map((post) => (
+          <div key={post.id} {...post}>
+            <p>{post.first} {post.last}: {post.comment}</p>
+            <p>{post.timestamp} | {post.id}</p>
+          </div>
+        ))}
+      </article> 
     </div>
   )
 }
